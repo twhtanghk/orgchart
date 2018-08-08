dc1d11a558e0908dd2da10c3629795ffe2738925
root <twhtanghk@gmail.com>
2018-08-08 16:53:29 +0800

    remove nocache for IE font download update style in App.vue

--- backend/config/http.coffee
+++ backend/config/http.coffee
@@ -2,10 +2,8 @@ module.exports =
   hookTimeout: 50000
   http:
     middleware:
-      nocache: require('nocache')()
       methodOverride: require('method-override')()
       order: [
-        'nocache'
         'bodyParser'
         'compress'
         'methodOverride'
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,10 +14,12 @@ export default
     tree: require('./tree').default
 </script>
 
-<style>
-@import 'https://use.fontawesome.com/releases/v5.0.9/css/all.css';
-@import '../node_modules/material-icons/iconfont/material-icons.css';
-@import 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css';
+<style lang='scss'>
+$material-icons-font-path: '~material-icons/iconfont/';
+
+@import '~material-icons/iconfont/material-icons.scss';
+@import url('https://use.fontawesome.com/releases/v5.0.9/css/all.css');
+@import url('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css');
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
